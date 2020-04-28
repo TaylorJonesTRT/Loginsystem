19f4a52523aed39728a28ca1e9fb2f4fce78e56e
Taylor Jones <taylor@Taylors-MBP.lan1>
2020-04-28 14:59:47 -0500

    Started on login portion of the system to identify if username and password combination exists or not

--- loginsystem.py
+++ loginsystem.py
@@ -3,4 +3,12 @@ user_details = {
 }
 
 print("Welcome to the new and sphoisticated login system!")
-print("What would you like to do? \n [1]LOGIN \n [2]REGISTER")
+choice = input("What would you like to do? \n [1]LOGIN \n [2]REGISTER \n")
+
+while choice == "1":
+    username = input("Pleaser enter your username: ")
+    if username in user_details.keys():
+        password = input("Please enter your password: ")
+    else:
+        print("Sorry, that username does not exist. Please try again")
+        continue
