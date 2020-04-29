c4c42b2433d98847caaa1fa91fb0f6b604ce6261
Taylor Jones <taylor@Taylors-MBP.lan1>
2020-04-29 15:14:06 -0500

    Finished the program. Commiting completed work

--- loginsystem.py
+++ loginsystem.py
@@ -1,36 +1,51 @@
-user_details = {
-    'Test': 'Test123'
-}
+import json
+import sys
+import hashlib
 
-print("Welcome to the new and sphoisticated login system!")
+# TODO: Incorporate while loops so that the program can go back to certain areas after the user has completed either the login or registration.
+
+user_details = {}
+
+hasher = hashlib.sha256()
+
+with open('user_credentials.json', 'r') as f:
+    user_details = json.load(f)
 
 while True:
+    print("Welcome to the new and sphoisticated login system!")
+
     choice = input("What would you like to do? \n [1]LOGIN \n [2]REGISTER \n")
+
     while choice == "1":
         username_login = input("Pleaser enter your username: ")
         if username_login in user_details.keys():
-            password_login = input("Please enter your password: ")
-            if (username_login in user_details and user_details[username_login] == password_login):
+            password_login = input(
+                "Please enter your password: ").encode('utf-8')
+            password_login = hasher.update(password_login)
+            if username_login in user_details and user_details[username_login] == password_login:
                 print("You are now loggged in! Welcome back!")
-                break
+                print("Now exiting program")
+                sys.exit()
             else:
                 print("Sorry, that was the wrong password. Try again.")
         else:
             print("Sorry, that username does not exist. Please try again")
-            continue
 
     while choice == "2":
         print("Please choose a username, it can be your email address or a plain username")
-        username_registragion = input("")
+        username_registration = input("")
         print("Please enter a password")
-        password_registration = input("")
-        user_details[username_registragion] = password_registration
+        password_registration = input("").encode('utf-8')
+        user_details[username_registration] = hasher.update(
+            password_registration)
+        with open('user_credentials.json', 'w') as fp:
+            json.dump(user_details, fp, indent=4)
         print("Thank you for registering! You may now sign in!")
         break
 
     decision = input("What would you like to do now? Continue or Exit?")
 
     if decision == "Continue" or "continue":
-        continue
+        pass
     else:
-        break
+        sys.exit()
